d799434f71b0a30d19f6749486ba01702ab3be4a
Ashok J <129277628+ashokbt2002@users.noreply.github.com>
2023-07-14 11:23:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('button');
+const body = document.querySelector('body');
+const color = ['pink','black','red','grey','yellow','purple'];
+body.style.backgroundColor = 'green';
+document.addEventListener('click',function(){
+    const colorIndex = parseInt(Math.random()*color.length);
+    body.style.backgroundColor = color[colorIndex];
+});
